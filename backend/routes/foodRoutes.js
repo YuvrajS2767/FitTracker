6b6e1f5b2188a6880757da6900b24d798911ff79
YuvrajS2767 <yuvrajsiwal14@gmail.com>
2025-02-14 13:01:24 +0530

    Delete backend directory

--- backend/routes/foodRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getNutritionData } = require("../controllers/foodController");
-
-const router = express.Router();
-
-router.post("/nutrition", getNutritionData);
-
-module.exports = router;
--- backend/utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
-
-export { generateToken };
